d93b5878273981072a7af4bcfa40253f2578c2e2
Santillan-Reyna-Ariel-Angel <santillanreynaarielangel@gmail.com>
2024-04-30 12:24:18 -0400

    Agregar archivos de proyecto.

---
+++ FormsTestDelete/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace FormsTestDelete
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(77, 32);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(161, 35);
+            this.btnConectar.TabIndex = 0;
+            this.btnConectar.Text = "conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 125);
+            this.Controls.Add(this.btnConectar);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConectar;
+    }
+}
+
---
+++ FormsTestDelete/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace FormsTestDelete
+{
+    public partial class Form1 : Form
+    {
+        //string cadenaConexion = "server=" + servidor + ";" + "port=" + puerto + ";" + "user id=" + usuario + ";" + "password=" + password + ";" + "database=" + bd + ";";
+        MySqlConnection conexion = new MySqlConnection(
+            "SERVER=LOCALHOST; DATABASE=conexion; UID=root; PASSWORD=Mysqlpassword_1"
+        );
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) { }
+
+        private MySqlConnection establecerConexion()
+        {
+            try
+            {
+                conexion.Open();
+                MySqlCommand cmd = new MySqlCommand(
+                    "SELECT usuario FROM usuarios WHERE user_id = 7",
+                    conexion
+                );
+
+                // Ejecutar la consulta y obtener el resultado
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    // Si hay resultados:
+
+                    string usuario = reader["usuario"].ToString(); // Almacenar el resultado en una variable
+                    Console.WriteLine(usuario); // Mostrar el resultado
+                    MessageBox.Show("Usuario encontrado: " + usuario); // Mostrar el resultado en un MessageBox
+                }
+                else
+                {
+                    Console.WriteLine("No se encontró el usuario.");
+                    MessageBox.Show("No se encontró el usuario.");
+                }
+
+                // No olvides cerrar el lector y la conexión
+                reader.Close();
+                conexion.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+
+            return conexion;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            establecerConexion();
+        }
+    }
+}
