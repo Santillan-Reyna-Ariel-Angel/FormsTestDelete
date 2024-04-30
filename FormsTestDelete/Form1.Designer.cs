c801002921711aa8a9e481dd3d15e3315d9accf7
Santillan-Reyna-Ariel-Angel <santillanreynaarielangel@gmail.com>
2024-04-30 12:40:46 -0400

    interfaz login v1

--- FormsTestDelete/Form1.Designer.cs
+++ FormsTestDelete/Form1.Designer.cs
@@ -29,11 +29,15 @@
         private void InitializeComponent()
         {
             this.btnConectar = new System.Windows.Forms.Button();
+            this.textBoxUser = new System.Windows.Forms.TextBox();
+            this.textBoxContrasenia = new System.Windows.Forms.TextBox();
+            this.labelUsuario = new System.Windows.Forms.Label();
+            this.labelContrasenia = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnConectar
             // 
-            this.btnConectar.Location = new System.Drawing.Point(77, 32);
+            this.btnConectar.Location = new System.Drawing.Point(143, 130);
             this.btnConectar.Name = "btnConectar";
             this.btnConectar.Size = new System.Drawing.Size(161, 35);
             this.btnConectar.TabIndex = 0;
@@ -41,23 +45,68 @@
             this.btnConectar.UseVisualStyleBackColor = true;
             this.btnConectar.Click += new System.EventHandler(this.button1_Click);
             // 
+            // textBoxUser
+            // 
+            this.textBoxUser.Location = new System.Drawing.Point(169, 12);
+            this.textBoxUser.Name = "textBoxUser";
+            this.textBoxUser.Size = new System.Drawing.Size(208, 22);
+            this.textBoxUser.TabIndex = 1;
+            this.textBoxUser.TextChanged += new System.EventHandler(this.textBoxUser_TextChanged);
+            // 
+            // textBoxContrasenia
+            // 
+            this.textBoxContrasenia.Location = new System.Drawing.Point(169, 50);
+            this.textBoxContrasenia.Name = "textBoxContrasenia";
+            this.textBoxContrasenia.Size = new System.Drawing.Size(208, 22);
+            this.textBoxContrasenia.TabIndex = 2;
+            this.textBoxContrasenia.TextChanged += new System.EventHandler(this.textBoxContrasenia_TextChanged);
+            // 
+            // labelUsuario
+            // 
+            this.labelUsuario.AutoSize = true;
+            this.labelUsuario.Location = new System.Drawing.Point(75, 15);
+            this.labelUsuario.Name = "labelUsuario";
+            this.labelUsuario.Size = new System.Drawing.Size(60, 16);
+            this.labelUsuario.TabIndex = 3;
+            this.labelUsuario.Text = "Usuario: ";
+            this.labelUsuario.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelContrasenia
+            // 
+            this.labelContrasenia.AutoSize = true;
+            this.labelContrasenia.Location = new System.Drawing.Point(75, 53);
+            this.labelContrasenia.Name = "labelContrasenia";
+            this.labelContrasenia.Size = new System.Drawing.Size(85, 16);
+            this.labelContrasenia.TabIndex = 4;
+            this.labelContrasenia.Text = "Contrasenia: ";
+            this.labelContrasenia.Click += new System.EventHandler(this.label2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(339, 125);
+            this.ClientSize = new System.Drawing.Size(437, 230);
+            this.Controls.Add(this.labelContrasenia);
+            this.Controls.Add(this.labelUsuario);
+            this.Controls.Add(this.textBoxContrasenia);
+            this.Controls.Add(this.textBoxUser);
             this.Controls.Add(this.btnConectar);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.TextBox textBoxUser;
+        private System.Windows.Forms.TextBox textBoxContrasenia;
+        private System.Windows.Forms.Label labelUsuario;
+        private System.Windows.Forms.Label labelContrasenia;
     }
 }
 
--- FormsTestDelete/Form1.cs
+++ FormsTestDelete/Form1.cs
@@ -1,12 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
 
@@ -69,5 +61,13 @@ namespace FormsTestDelete
         {
             establecerConexion();
         }
+
+        private void textBoxUser_TextChanged(object sender, EventArgs e) { }
+
+        private void textBoxContrasenia_TextChanged(object sender, EventArgs e) { }
+
+        private void label1_Click(object sender, EventArgs e) { }
+
+        private void label2_Click(object sender, EventArgs e) { }
     }
 }
