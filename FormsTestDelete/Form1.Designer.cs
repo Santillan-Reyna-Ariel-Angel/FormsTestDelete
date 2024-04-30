cce0e358d9ddc7299be773d5f064e27f2bc110be
Santillan-Reyna-Ariel-Angel <santillanreynaarielangel@gmail.com>
2024-04-30 14:39:16 -0400

    login v2

--- FormsTestDelete/Form1.Designer.cs
+++ FormsTestDelete/Form1.Designer.cs
@@ -28,72 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnConectar = new System.Windows.Forms.Button();
-            this.textBoxUser = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxUsuario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnIngresar = new System.Windows.Forms.Button();
             this.textBoxContrasenia = new System.Windows.Forms.TextBox();
-            this.labelUsuario = new System.Windows.Forms.Label();
-            this.labelContrasenia = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // btnConectar
+            // label1
             // 
-            this.btnConectar.Location = new System.Drawing.Point(143, 130);
-            this.btnConectar.Name = "btnConectar";
-            this.btnConectar.Size = new System.Drawing.Size(161, 35);
-            this.btnConectar.TabIndex = 0;
-            this.btnConectar.Text = "conectar";
-            this.btnConectar.UseVisualStyleBackColor = true;
-            this.btnConectar.Click += new System.EventHandler(this.button1_Click);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuario: ";
             // 
-            // textBoxUser
+            // textBoxUsuario
             // 
-            this.textBoxUser.Location = new System.Drawing.Point(169, 12);
-            this.textBoxUser.Name = "textBoxUser";
-            this.textBoxUser.Size = new System.Drawing.Size(208, 22);
-            this.textBoxUser.TabIndex = 1;
-            this.textBoxUser.TextChanged += new System.EventHandler(this.textBoxUser_TextChanged);
+            this.textBoxUsuario.Location = new System.Drawing.Point(131, 17);
+            this.textBoxUsuario.Name = "textBoxUsuario";
+            this.textBoxUsuario.Size = new System.Drawing.Size(278, 22);
+            this.textBoxUsuario.TabIndex = 1;
+            this.textBoxUsuario.TextChanged += new System.EventHandler(this.textBoxUsuario_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Contrasenia: ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(286, 109);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(123, 23);
+            this.btnIngresar.TabIndex = 4;
+            this.btnIngresar.Text = "INGRESAR";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
             // 
             // textBoxContrasenia
             // 
-            this.textBoxContrasenia.Location = new System.Drawing.Point(169, 50);
+            this.textBoxContrasenia.Location = new System.Drawing.Point(131, 61);
             this.textBoxContrasenia.Name = "textBoxContrasenia";
-            this.textBoxContrasenia.Size = new System.Drawing.Size(208, 22);
-            this.textBoxContrasenia.TabIndex = 2;
+            this.textBoxContrasenia.PasswordChar = '*';
+            this.textBoxContrasenia.Size = new System.Drawing.Size(278, 22);
+            this.textBoxContrasenia.TabIndex = 5;
             this.textBoxContrasenia.TextChanged += new System.EventHandler(this.textBoxContrasenia_TextChanged);
-            // 
-            // labelUsuario
-            // 
-            this.labelUsuario.AutoSize = true;
-            this.labelUsuario.Location = new System.Drawing.Point(75, 15);
-            this.labelUsuario.Name = "labelUsuario";
-            this.labelUsuario.Size = new System.Drawing.Size(60, 16);
-            this.labelUsuario.TabIndex = 3;
-            this.labelUsuario.Text = "Usuario: ";
-            this.labelUsuario.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // labelContrasenia
-            // 
-            this.labelContrasenia.AutoSize = true;
-            this.labelContrasenia.Location = new System.Drawing.Point(75, 53);
-            this.labelContrasenia.Name = "labelContrasenia";
-            this.labelContrasenia.Size = new System.Drawing.Size(85, 16);
-            this.labelContrasenia.TabIndex = 4;
-            this.labelContrasenia.Text = "Contrasenia: ";
-            this.labelContrasenia.Click += new System.EventHandler(this.label2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(437, 230);
-            this.Controls.Add(this.labelContrasenia);
-            this.Controls.Add(this.labelUsuario);
+            this.ClientSize = new System.Drawing.Size(469, 230);
             this.Controls.Add(this.textBoxContrasenia);
-            this.Controls.Add(this.textBoxUser);
-            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxUsuario);
+            this.Controls.Add(this.label1);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Login";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -102,11 +102,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Button btnConectar;
-        private System.Windows.Forms.TextBox textBoxUser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxUsuario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnIngresar;
         private System.Windows.Forms.TextBox textBoxContrasenia;
-        private System.Windows.Forms.Label labelUsuario;
-        private System.Windows.Forms.Label labelContrasenia;
     }
 }
 
--- FormsTestDelete/Form1.cs
+++ FormsTestDelete/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.Eventing.Reader;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
 
@@ -6,27 +7,57 @@ namespace FormsTestDelete
 {
     public partial class Form1 : Form
     {
+        public string usuario;
+        public string contrasenia;
+        public string usuarioId;
+
         //string cadenaConexion = "server=" + servidor + ";" + "port=" + puerto + ";" + "user id=" + usuario + ";" + "password=" + password + ";" + "database=" + bd + ";";
         MySqlConnection conexion = new MySqlConnection(
             "SERVER=LOCALHOST; DATABASE=conexion; UID=root; PASSWORD=Mysqlpassword_1"
         );
 
+        // Constructor de la clase:
         public Form1()
         {
+            // InitializeComponent(): Inicializa todos los controles colocados en el formulario a través del diseñador de Windows Forms
             InitializeComponent();
         }
 
+        // Funcion que se ejecuta automaticamente antes que el formulario se muestre por 1ra vez (para cargar datos que se mostraran al usuario/interfaz):
         private void Form1_Load(object sender, EventArgs e) { }
 
-        private MySqlConnection establecerConexion()
+        private void label2_Click(object sender, EventArgs e) { }
+
+        private void textBoxUsuario_TextChanged(object sender, EventArgs e)
         {
+            string inputUsuario = textBoxUsuario.Text; // Capturamos el texto del TextBox
+            usuario = inputUsuario;
+        }
+
+        private void textBoxContrasenia_TextChanged(object sender, EventArgs e)
+        {
+            string inputContrasenia = textBoxContrasenia.Text;
+            contrasenia = inputContrasenia;
+        }
+
+        private bool validarUsuario()
+        {
+            bool usuarioValido;
+
             try
             {
+                // Abrir la conexion:
                 conexion.Open();
+
+                // Consulta sql con parametros (@x) :
                 MySqlCommand cmd = new MySqlCommand(
-                    "SELECT usuario FROM usuarios WHERE user_id = 7",
+                    "SELECT * FROM usuarios WHERE usuario = @usuario and contrasena = @contrasenia",
                     conexion
                 );
+
+                // Agregar los parámetros al comando y asignar sus valores( o variable):
+                cmd.Parameters.AddWithValue("@usuario", usuario);
+                cmd.Parameters.AddWithValue("@contrasenia", contrasenia);
 
                 // Ejecutar la consulta y obtener el resultado
                 MySqlDataReader reader = cmd.ExecuteReader();
@@ -34,40 +65,46 @@ namespace FormsTestDelete
                 if (reader.Read())
                 {
                     // Si hay resultados:
-
-                    string usuario = reader["usuario"].ToString(); // Almacenar el resultado en una variable
-                    Console.WriteLine(usuario); // Mostrar el resultado
-                    MessageBox.Show("Usuario encontrado: " + usuario); // Mostrar el resultado en un MessageBox
+                    string usuarioIdBD = reader["user_id"].ToString(); // Almacenar el resultado en una variable
+                    string usuarioBD = reader["usuario"].ToString();
+                    usuario = usuarioBD;
+                    usuarioId = usuarioIdBD;
+                    usuarioValido = true;
                 }
                 else
                 {
-                    Console.WriteLine("No se encontró el usuario.");
-                    MessageBox.Show("No se encontró el usuario.");
+                    // No hay resultados:
+                    usuarioValido = false;
                 }
 
-                // No olvides cerrar el lector y la conexión
+                // Cerrar el lector y la conexión
                 reader.Close();
                 conexion.Close();
             }
             catch (Exception ex)
             {
+                usuarioValido = false;
                 MessageBox.Show("Error: " + ex.Message);
             }
 
-            return conexion;
+            return usuarioValido;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btnIngresar_Click(object sender, EventArgs e)
         {
-            establecerConexion();
+            bool usuarioValido;
+            usuarioValido = validarUsuario();
+
+            if (usuarioValido == true)
+            {
+                MessageBox.Show("Se encontró el usuario.");
+                //Console.WriteLine("Se encontró el usuario.");
+            }
+            else
+            {
+                MessageBox.Show("No se encontró el usuario.");
+                //Console.WriteLine("No se encontró el usuario.");
+            }
         }
-
-        private void textBoxUser_TextChanged(object sender, EventArgs e) { }
-
-        private void textBoxContrasenia_TextChanged(object sender, EventArgs e) { }
-
-        private void label1_Click(object sender, EventArgs e) { }
-
-        private void label2_Click(object sender, EventArgs e) { }
     }
 }
